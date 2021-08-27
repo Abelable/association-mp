18df3856b26eabd2909de722c75bfb3a56b507a0
fangkai <1209683835@qq.com>
2021-08-27 14:16:51 +0800

    feat: 文章列表

--- pages/index/components/i-news-lists/index.js
+++ pages/index/components/i-news-lists/index.js
@@ -5,8 +5,13 @@ Component({
     item: {
       type: Object,
       observer(info) {
-        const { virtual_like, actual_like, virtual_look, actual_look } = info || {}
+        let { content, virtual_like, actual_like, virtual_look, actual_look } = info || {}
+        content = content.replace(/<\/?[^>]*>/g,'') //去除HTML tag
+        content = content.replace(/[ | ]*\n/g,'\n') //去除行尾空白
+        content = content.replace(/\n[\s| | ]*\r/g,'\n') //去除多余空行
+        content = content.replace(/ /ig,'') //去掉 
         this.setData({
+          content,
           praiseCount: Number(virtual_like) + Number(actual_like),
           viewsCount: Number(virtual_look) + Number(actual_look)
         })
@@ -15,6 +20,7 @@ Component({
   },
 
   data: {
+    content: '',
     praiseCount: 0,
     viewsCount: 0
   },
