4995875ec09359d8cd1cf60479799f3feff9c222
fangkai <1209683835@qq.com>
2021-09-16 10:24:39 +0800

    fix: 修复首页新闻列表内容简介文案显示问题

--- pages/index/components/i-news-lists/index.js
+++ pages/index/components/i-news-lists/index.js
@@ -10,13 +10,11 @@ Component({
         content = content.replace(/<\/?[^>]*>/g,'') //去除HTML tag
         content = content.replace(/[ | ]*\n/g,'\n') //去除行尾空白
         content = content.replace(/\n[\s| | ]*\r/g,'\n') //去除多余空行
-        content = content.replace(/ /ig,'') //去掉 
-        content = content.replace(/&nbsp/g,'')
-        content = content.replace(/&ldquo/g,'')
-        content = content.replace(/&lsquo/g,'')
-        content = content.replace(/&rsquo/g,'')
-        content = content.replace(/&rdquo/g,'')
-        content = content.replace(/;;/g,'')
+        content = content.replace(/&nbsp;/g, '')
+        content = content.replace(/&ldquo;/g, '“')
+        content = content.replace(/&rdquo;/g, '”')
+        content = content.replace(/&lsquo;/g, '’')
+        content = content.replace(/&rsquo;/g, '’')
         this.setData({
           content,
           praiseCount: Number(virtual_like) + Number(actual_like),
