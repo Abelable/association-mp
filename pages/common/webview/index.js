6296c886e19e5d577b9b6427490fe6f04a79a0ce
fangkai <1209683835@qq.com>
2022-04-28 14:53:24 +0800

    feat: 头图

--- pages/common/webview/index.js
+++ pages/common/webview/index.js
@@ -4,6 +4,11 @@ Page({
   },
 
   onLoad(options) {
-    this.setData({ url: options.url })
+    let { url, title, ...rest } = options
+    wx.setNavigationBarTitle({ title })
+    for (let key in rest) {
+      url += `${url.indexOf('?') === -1 ? '?' : '&'}${key}=${rest[key]}`
+    }
+    this.setData({ url })
   }
 })
