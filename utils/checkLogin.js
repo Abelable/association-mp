f863dc6ae98f19fc148194204309cd948755cdd6
Administrator <admin@example.com>
2023-02-07 16:13:06 +0800

    feat: 登录

--- utils/checkLogin.js
+++ utils/checkLogin.js
@@ -3,15 +3,15 @@ import BaseService from '../service/baseService'
 const baseService = new BaseService()
 
 const login = async () => {
-  // const { userInfo } = await baseService.getUserProfile()
+  const { userInfo } = await baseService.getUserProfile()
   const { code } = await baseService.wxLogin()
   const { open_id, is_bind } = await baseService.login(code)
 
   wx.setStorageSync('openid', open_id)
-  // wx.setStorageSync('userInfo', userInfo)
-  // if (is_bind == 0) {
-  //   await baseService.bindUserInfo(userInfo)
-  // }
+  wx.setStorageSync('userInfo', userInfo)
+  if (is_bind == 0) {
+    await baseService.bindUserInfo(userInfo)
+  }
 }
 
 const checkLogin = async (fn) => {
