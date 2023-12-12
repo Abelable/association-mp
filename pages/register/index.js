74ec9cd4ce84812720a812d58fa10fd684fef043
Administrator <admin@example.com>
2023-12-12 10:18:26 +0800

    feat: 会员入驻

--- pages/register/index.js
+++ pages/register/index.js
@@ -1,8 +1,8 @@
-import checkLogin from '../../utils/checkLogin'
-import RegisterService from './utils/registerService'
+import checkLogin from "../../utils/checkLogin";
+import RegisterService from "./utils/registerService";
 
-const registerService = new RegisterService()
-const { statusBarHeight } = getApp().globalData
+const registerService = new RegisterService();
+const { statusBarHeight } = getApp().globalData;
 
 const categoryOptions = [
   {
@@ -58,67 +58,72 @@ const categoryOptions = [
     ],
   },
   { text: "金融服务类平台", value: 5 },
-  { text: "其他类", value: 7 },
   { text: "信息资讯类平台", value: 6 },
+  { text: "其他类", value: 7 },
 ];
 
 Page({
   data: {
     statusBarHeight,
     categoryOptions,
-    companyType: -1,
-    companyName: '', 
-    companyShortName: '', 
+    curCategoryIdx: -1,
+    companyName: "",
+    companyShortName: "",
     region: [],
-    websiteUrl: '', 
-    ICP: '', 
-    staffCount: '',
-    gangCount: '',
-    tradeAmount: '',
-    revenue: '',
-    name: '',
-    contacterName: '', 
-    contacterJobTitle: '', 
-    contacterTel: '', 
-    licenseImg: '',
+    websiteUrl: "",
+    ICP: "",
+    staffCount: "",
+    gangCount: "",
+    userCount: "",
+    merchantCount: "",
+    tradeAmount: "",
+    revenue: "",
+    taxAmount: "",
+    name: "",
+    contacterName: "",
+    contacterJobTitle: "",
+    contacterTel: "",
+    licenseImg: "",
     fileList: [],
     logoList: [],
-    minePopupVisible: false
+    minePopupVisible: false,
   },
 
   setCompanyName(e) {
-    this.companyName = e.detail.value
+    this.companyName = e.detail.value;
   },
 
   setCompanyShortName(e) {
-    this.companyShortName = e.detail.value
+    this.companyShortName = e.detail.value;
   },
 
   bindRegionChange(e) {
-    const { value, code } = e.detail
+    const { value, code } = e.detail;
     this.address = {
       province: value[0],
       city: value[1],
       area: value[2],
-      region: [code[2].slice(0, 2), code[2].slice(0, 4), code[2]]
-    }
+      region: [code[2].slice(0, 2), code[2].slice(0, 4), code[2]],
+    };
     this.setData({
-      region: value
-    })
+      region: value,
+    });
   },
 
   setWebsiteUrl(e) {
-    this.websiteUrl = e.detail.value
+    this.websiteUrl = e.detail.value;
   },
 
   setICP(e) {
-    this.ICP = e.detail.value
+    this.ICP = e.detail.value;
   },
 
   setCompanyType(e) {
-    const companyType = Number(e.detail.value);
-    this.setData({ companyType })
-    console.log('subOptions', categoryOptions[companyType])
+    this.companyType = Number(e.detail.value);
+    const curCategoryIdx = categoryOptions.findIndex(
+      (item) => item.value === this.companyType
+    );
+    this.setData({ curCategoryIdx });
   },
 
   setCompanySubType(e) {
@@ -126,175 +131,248 @@ Page({
   },
 
   setStaffCount(e) {
-    this.staffCount = e.detail.value
+    this.staffCount = e.detail.value;
   },
 
   setGangCount(e) {
-    this.gangCount = e.detail.value
+    this.gangCount = e.detail.value;
+  },
+
+  setUserCount(e) {
+    this.userCount = e.detail.value;
+  },
+
+  setMerchantCount(e) {
+    this.merchantCount = e.detail.value;
   },
 
   setTradeAmount(e) {
-    this.tradeAmount = e.detail.value
+    this.tradeAmount = e.detail.value;
+  },
+
+  setTaxAmount(e) {
+    this.taxAmount = e.detail.value;
   },
 
   setRevenue(e) {
-    this.revenue = e.detail.value
+    this.revenue = e.detail.value;
   },
 
   setContacterName(e) {
-    this.contacterName = e.detail.value
+    this.contacterName = e.detail.value;
   },
 
   setContacterJobTitle(e) {
-    this.contacterJobTitle = e.detail.value
+    this.contacterJobTitle = e.detail.value;
   },
 
   setContacterTel(e) {
-    this.contacterTel = e.detail.value
+    this.contacterTel = e.detail.value;
   },
 
   async afterRead(e) {
-    const { file } = e.detail
-    const licenseImg = await registerService.uploadImg(file.url)
+    const { file } = e.detail;
+    const licenseImg = await registerService.uploadImg(file.url);
     this.setData({
-      fileList: [...this.data.fileList, { ...file, url: licenseImg }]
-    })
+      fileList: [...this.data.fileList, { ...file, url: licenseImg }],
+    });
   },
 
   async afterLogoRead(e) {
-    const { file } = e.detail
-    const logoImg = await registerService.uploadImg(file.url)
+    const { file } = e.detail;
+    const logoImg = await registerService.uploadImg(file.url);
     this.setData({
-      logoList: [{ ...file, url: logoImg }]
-    })
+      logoList: [{ ...file, url: logoImg }],
+    });
   },
 
   deleteFile(e) {
-    const { index } = e.detail
-    let { fileList } = this.data
-    fileList.splice(index, 1)
-    this.setData({ fileList })
+    const { index } = e.detail;
+    let { fileList } = this.data;
+    fileList.splice(index, 1);
+    this.setData({ fileList });
   },
 
   deleteLogo() {
-    this.setData({ logoList: [] })
+    this.setData({ logoList: [] });
   },
 
   async uploadLicense() {
-    const { tempFilePaths } = await registerService.chooseImage(1)
-    const licenseImg = await registerService.uploadImg(tempFilePaths[0])
-    this.setData({ licenseImg })
+    const { tempFilePaths } = await registerService.chooseImage(1);
+    const licenseImg = await registerService.uploadImg(tempFilePaths[0]);
+    this.setData({ licenseImg });
   },
 
   async submit() {
     checkLogin(() => {
-      const { companyName, companyShortName, address, websiteUrl, ICP, companyType, staffCount, gangCount, tradeAmount, revenue, contacterName, contacterJobTitle, contacterTel } = this
-      const { fileList, logoList } = this.data
-      
+      const {
+        companyName,
+        companyShortName,
+        address,
+        websiteUrl,
+        ICP,
+        companyType,
+        companySubType,
+        staffCount,
+        gangCount,
+        userCount,
+        merchantCount,
+        tradeAmount,
+        revenue,
+        taxAmount,
+        contacterName,
+        contacterJobTitle,
+        contacterTel,
+      } = this;
+      const { fileList, logoList, curCategoryIdx } = this.data;
+
       if (!companyName) {
-        wx.showToast({ title: '请输入企业名称', icon: 'none' })
-        return
+        wx.showToast({ title: "请输入企业名称", icon: "none" });
+        return;
       }
       if (!companyShortName) {
-        wx.showToast({ title: '请输入企业简称', icon: 'none' })
-        return
+        wx.showToast({ title: "请输入企业简称", icon: "none" });
+        return;
       }
       if (!address) {
-        wx.showToast({ title: '请选择企业所在地区', icon: 'none' })
-        return
+        wx.showToast({ title: "请选择企业所在地区", icon: "none" });
+        return;
       }
       if (!websiteUrl) {
-        wx.showToast({ title: '网站（app）名称', icon: 'none' })
-        return
+        wx.showToast({ title: "网站（app）名称", icon: "none" });
+        return;
       }
       if (!ICP) {
-        wx.showToast({ title: '请输入ICP备案号', icon: 'none' })
-        return
+        wx.showToast({ title: "请输入ICP备案号", icon: "none" });
+        return;
       }
       if (!companyType) {
-        wx.showToast({ title: '请选择企业类型', icon: 'none' })
-        return
+        wx.showToast({ title: "请选择企业类型", icon: "none" });
+        return;
+      }
+      if (categoryOptions[curCategoryIdx].subOptions && !companySubType) {
+        wx.showToast({ title: "请选择企业二级类型", icon: "none" });
+        return;
       }
       if (!tradeAmount) {
-        wx.showToast({ title: '请输入上年度GMV', icon: 'none' })
-        return
+        wx.showToast({ title: "请输入上年度GMV", icon: "none" });
+        return;
       }
       if (!revenue) {
-        wx.showToast({ title: '请输入上年度应收', icon: 'none' })
-        return
+        wx.showToast({ title: "请输入上年度应收", icon: "none" });
+        return;
+      }
+      if (!taxAmount) {
+        wx.showToast({ title: "请输入上年度纳税额", icon: "none" });
+        return;
       }
       if (!staffCount) {
-        wx.showToast({ title: '请输入员工人数', icon: 'none' })
-        return
+        wx.showToast({ title: "请输入员工人数", icon: "none" });
+        return;
       }
       if (!gangCount) {
-        wx.showToast({ title: '请输入党员人数', icon: 'none' })
-        return
+        wx.showToast({ title: "请输入党员人数", icon: "none" });
+        return;
+      }
+      if (!userCount) {
+        wx.showToast({ title: "请输入用户数量", icon: "none" });
+        return;
+      }
+      if (!merchantCount) {
+        wx.showToast({ title: "请输入商家数量", icon: "none" });
+        return;
       }
       if (!contacterName) {
-        wx.showToast({ title: '请输入联系人姓名', icon: 'none' })
-        return
+        wx.showToast({ title: "请输入联系人姓名", icon: "none" });
+        return;
       }
       if (!contacterJobTitle) {
-        wx.showToast({ title: '请输入联系人职务', icon: 'none' })
-        return
+        wx.showToast({ title: "请输入联系人职务", icon: "none" });
+        return;
       }
-      if (!contacterTel || !(/^1[3456789]\d{9}$/.test(this.contacterTel))) {
-        wx.showToast({ title: '请输入正确的工作联系方式', icon: 'none' })
-        return
+      if (!contacterTel || !/^1[3456789]\d{9}$/.test(this.contacterTel)) {
+        wx.showToast({ title: "请输入正确的工作联系方式", icon: "none" });
+        return;
       }
 
       if (!fileList.length) {
-        wx.showToast({ title: '请上传企业营业执照或副本', icon: 'none' })
-        return
+        wx.showToast({ title: "请上传企业营业执照或副本", icon: "none" });
+        return;
       }
 
-      const licenseImgs = []
-      fileList.forEach(item => {
-        licenseImgs.push(item.url)
-      })
+      const licenseImgs = [];
+      fileList.forEach((item) => {
+        licenseImgs.push(item.url);
+      });
 
-      const content = { companyName, companyShortName, address: JSON.stringify(address), websiteUrl, ICP, companyType, staffCount, gangCount, tradeAmount, revenue, contacterName, contacterJobTitle, contacterTel, licenseImg: licenseImgs.join(), logoImg: logoList.length ? logoList[0].url : '' }
-      registerService.submitApply(content, () => {
-        wx.showToast({ title: '提交成功', icon: 'none' })
-        this.resetData()
-      }, () => {
-        wx.showToast({ title: '提交失败，请重试', icon: 'none' })
-      })
-    })
+      const content = {
+        companyName,
+        companyShortName,
+        address: JSON.stringify(address),
+        websiteUrl,
+        ICP,
+        companyType,
+        companySubType,
+        staffCount,
+        gangCount,
+        userCount,
+        merchantCount,
+        tradeAmount,
+        revenue,
+        taxAmount,
+        contacterName,
+        contacterJobTitle,
+        contacterTel,
+        licenseImg: licenseImgs.join(),
+        logoImg: logoList.length ? logoList[0].url : "",
+      };
+      registerService.submitApply(
+        content,
+        () => {
+          wx.showToast({ title: "提交成功", icon: "none" });
+          this.resetData();
+        },
+        () => {
+          wx.showToast({ title: "提交失败，请重试", icon: "none" });
+        }
+      );
+    });
   },
 
   resetData() {
     this.setData({
-      companyName: '',
-      companyShortName: '',
-      websiteUrl: '', 
-      ICP: '', 
-      staffCount: '',
-      gangCount: '',
-      tradeAmount: '',
-      revenue: '',
-      contacterName: '', 
-      contacterJobTitle: '', 
-      contacterTel: '', 
-      licenseImg: '',
+      companyName: "",
+      companyShortName: "",
+      websiteUrl: "",
+      ICP: "",
+      staffCount: "",
+      gangCount: "",
+      userCount: "",
+      merchantCount: "",
+      tradeAmount: "",
+      revenue: "",
+      taxAmount: "",
+      contacterName: "",
+      contacterJobTitle: "",
+      contacterTel: "",
+      licenseImg: "",
       fileList: [],
       logoList: [],
       region: [],
-    })
+    });
   },
 
   showMinePopup() {
     checkLogin(() => {
       this.setData({
-        minePopupVisible: true
-      })
-    })
+        minePopupVisible: true,
+      });
+    });
   },
 
   hideMinePopup() {
     this.setData({
-      minePopupVisible: false
-    })
+      minePopupVisible: false,
+    });
   },
-})
+});
--- pages/register/utils/registerService.js
+++ pages/register/utils/registerService.js
@@ -1,62 +1,136 @@
-import api from '../../../service/base/api'
-import BaseService from '../../../service/baseService'
+import api from "../../../service/base/api";
+import BaseService from "../../../service/baseService";
 
 class RegisterService extends BaseService {
   async uploadImg(filePath) {
-    wx.showLoading({ title: '图片上传中...' })
-    const _filePath = `data:image/jpeg;base64,${wx.getFileSystemManager().readFileSync(filePath, "base64")}`
-    const res = await api.request({ 
-      url: 'https://mms.youboi.com/api/v4/user/material',
-      method: 'POST', 
-      data: { 
-        'file[content]': _filePath,
-        wxmini: -1
+    wx.showLoading({ title: "图片上传中..." });
+    const _filePath = `data:image/jpeg;base64,${wx
+      .getFileSystemManager()
+      .readFileSync(filePath, "base64")}`;
+    const res = await api.request({
+      url: "https://mms.youboi.com/api/v4/user/material",
+      method: "POST",
+      data: {
+        "file[content]": _filePath,
+        wxmini: -1,
       },
-      header: { 
-        "content-type": 'application/x-www-form-urlencoded'
-      }
-    })
-    wx.hideLoading()
-    if (res.statusCode === 200 && res.data.code === 1001) return res.data.data[0]
+      header: {
+        "content-type": "application/x-www-form-urlencoded",
+      },
+    });
+    wx.hideLoading();
+    if (res.statusCode === 200 && res.data.code === 1001)
+      return res.data.data[0];
   }
 
-  async submitApply(content, success, fail, id = '') {
-    const { companyName, companyShortName, address, websiteUrl, ICP, companyType, websiteType, staffCount, gangCount, tradeCommodity, revenue, tradeAmount, tel, email, contacterName, contacterJobTitle, contacterTel, licenseImg, logoImg, memberCount, operatorCount } = content
+  async submitApply(content, success, fail, id = "") {
+    const {
+      companyName,
+      companyShortName,
+      address,
+      websiteUrl,
+      ICP,
+      companyType,
+      companySubType,
+      websiteType,
+      staffCount,
+      gangCount,
+      userCount,
+      merchantCount,
+      tradeCommodity,
+      revenue,
+      tradeAmount,
+      taxAmount,
+      tel,
+      email,
+      contacterName,
+      contacterJobTitle,
+      contacterTel,
+      licenseImg,
+      logoImg,
+      memberCount,
+      operatorCount,
+    } = content;
     const applyContent = [
-      { "title":"logo","name":"logo","value": logoImg },
-      { "title": '企业名称', "name": 'company_name', "value": companyName },
-      { "title": '企业简称', "name": 'short_name', "value": companyShortName },
-      { "title": '企业所在地区', "name": 'address', "value": address },
-      { "title": '网站（app）名称', "name": 'website_url', "value": websiteUrl },
-      { "title": 'ICP备案号', "name": 'ICP', "value": ICP },
-      { "title": '企业类型', "name": 'company_type', "value": companyType },
-      { "title": '网站电子商务类型', "name": 'website_type', "value": websiteType },
-      { "title": '员工人数', "name": 'staff_count', "value": staffCount },
-      { "title": '党员人数', "name": 'gang_count', "value": gangCount },
-      { "title": '交易商品（服务）', "name": 'trade_commodity', "value": tradeCommodity },
-      { "title": '上年度营收', "name": 'revenue', "value": revenue },
-      { "title": '上年度GMV', "name": 'trade_amount', "value": tradeAmount },
-      { "title": '手机号', "name": '_mobile', "value": tel },
-      { "title": '邮箱', "name": '_email', "value": email },
-      { "title": '协会联系人姓名', "name": 'contacter_name', "value": contacterName },
-      { "title": '协会联系人职务', "name": 'contacter_job_title', "value": contacterJobTitle },
-      { "title": '协会联系人手机号', "name": 'contacter_mobile', "value": contacterTel },
-      { "title": '企业营业执照或副本', "name": 'license', "value": licenseImg },
-      { "title": '注册会员数量', "name": 'member_count', "value": memberCount },
-      { "title": '平台网站内经营者数量', "name": 'operator_count', "value": operatorCount }
-    ]
+      { title: "logo", name: "logo", value: logoImg },
+      { title: "企业名称", name: "company_name", value: companyName },
+      { title: "企业简称", name: "short_name", value: companyShortName },
+      { title: "企业所在地区", name: "address", value: address },
+      { title: "网站（app）名称", name: "website_url", value: websiteUrl },
+      { title: "ICP备案号", name: "ICP", value: ICP },
+      { title: "企业类型", name: "company_type", value: companyType },
+      {
+        title: "企业二级类型",
+        name: "company_sub_type",
+        value: companySubType,
+      },
+      { title: "网站电子商务类型", name: "website_type", value: websiteType },
+      { title: "员工人数", name: "staff_count", value: staffCount },
+      { title: "党员人数", name: "gang_count", value: gangCount },
+      { title: "用户数量", name: "user_count", value: userCount },
+      { title: "商户数量", name: "merchant_count", value: merchantCount },
+      {
+        title: "交易商品（服务）",
+        name: "trade_commodity",
+        value: tradeCommodity,
+      },
+      { title: "上年度营收", name: "revenue", value: revenue },
+      { title: "上年度GMV", name: "trade_amount", value: tradeAmount },
+      { title: "上年度纳税额", name: "tax_amount", value: taxAmount },
+      { title: "手机号", name: "_mobile", value: tel },
+      { title: "邮箱", name: "_email", value: email },
+      { title: "协会联系人姓名", name: "contacter_name", value: contacterName },
+      {
+        title: "协会联系人职务",
+        name: "contacter_job_title",
+        value: contacterJobTitle,
+      },
+      {
+        title: "协会联系人手机号",
+        name: "contacter_mobile",
+        value: contacterTel,
+      },
+      { title: "企业营业执照或副本", name: "license", value: licenseImg },
+      { title: "注册会员数量", name: "member_count", value: memberCount },
+      {
+        title: "平台网站内经营者数量",
+        name: "operator_count",
+        value: operatorCount,
+      },
+    ];
     if (id) {
-      return await this.post({ url: `${this.baseUrl}/api/v1/enter-apply/update-apply`, data: { title: '入会申请', id, apply_content_json: JSON.stringify(applyContent) }, loadingTitle: '提交中...', success, fail })
+      return await this.post({
+        url: `${this.baseUrl}/api/v1/enter-apply/update-apply`,
+        data: {
+          title: "入会申请",
+          id,
+          apply_content_json: JSON.stringify(applyContent),
+        },
+        loadingTitle: "提交中...",
+        success,
+        fail,
+      });
     } else {
-      return await this.post({ url: `${this.baseUrl}/api/v1/enter-apply/apply`, data: { title: '入会申请', apply_content_json: JSON.stringify(applyContent) }, loadingTitle: '提交中...', success, fail })
+      return await this.post({
+        url: `${this.baseUrl}/api/v1/enter-apply/apply`,
+        data: {
+          title: "入会申请",
+          apply_content_json: JSON.stringify(applyContent),
+        },
+        loadingTitle: "提交中...",
+        success,
+        fail,
+      });
     }
   }
 
   async getApplyDetail(id) {
-    return await this.get({ url: `${this.baseUrl}/api/v1/enter-apply/info-apply`, data: { id }, loadingTitle: '加载中...' })
+    return await this.get({
+      url: `${this.baseUrl}/api/v1/enter-apply/info-apply`,
+      data: { id },
+      loadingTitle: "加载中...",
+    });
   }
-
-  
 }
 
-export default RegisterService
+export default RegisterService;
