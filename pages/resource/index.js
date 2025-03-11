499052ae78bba359b0754eba609461ff6c85d0e6
funguy <1209683835@qq.com>
2025-03-11 17:54:50 +0800

    feat: 资源中心

--- pages/resource/index.js
+++ pages/resource/index.js
@@ -5,7 +5,8 @@ Page({
     statusBarHeight,
     curMenuIdx: 0,
     subMenuList: ["全部", "电商", "直播", "农产品", "AI", "制造业", "机械", "汽车"],
-    curSubMenuIdx: 0
+    curSubMenuIdx: 0,
+    categoryPickerModalVisible: false
   },
 
   onLoad(options) {},
@@ -19,4 +20,17 @@ Page({
     const curSubMenuIdx = e.currentTarget.dataset.index;
     this.setData({ curSubMenuIdx });
   },
+
+  showCategoryPickerModal() {
+    this.setData({ categoryPickerModalVisible: true });
+  },
+
+  confirmCategoryPick(e) {
+    const curSubMenuIdx = e.detail;
+    this.setData({ curSubMenuIdx, categoryPickerModalVisible: false });
+  },
+
+  hideCategoryPickerModal() {
+    this.setData({ categoryPickerModalVisible: false });
+  }
 });
