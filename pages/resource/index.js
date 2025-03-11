45497bf33839e3c4f1ba487392cfc412c71306fe
funguy <1209683835@qq.com>
2025-03-11 18:17:39 +0800

    feat: 资源中心

--- pages/resource/index.js
+++ pages/resource/index.js
@@ -4,9 +4,41 @@ Page({
   data: {
     statusBarHeight,
     curMenuIdx: 0,
-    subMenuList: ["全部", "电商", "直播", "农产品", "AI", "制造业", "机械", "汽车"],
+    subMenuList: [
+      "全部",
+      "电商",
+      "直播",
+      "农产品",
+      "AI",
+      "制造业",
+      "机械",
+      "汽车"
+    ],
     curSubMenuIdx: 0,
-    categoryPickerModalVisible: false
+    categoryPickerModalVisible: false,
+    enterpriseList: [
+      {
+        cover:
+          "https://img-oss.zjseca.com/government/20221122/1669105837384.png",
+        name: "杭州艺福堂茶叶有限公司",
+        type: "普通会员单位",
+        business: "茶叶销售、茶叶制品"
+      },
+      {
+        cover:
+          "https://img-oss.zjseca.com/government/20221122/1669105837384.png",
+        name: "杭州艺福堂茶叶有限公司",
+        type: "普通会员单位",
+        business: "茶叶销售、茶叶制品"
+      },
+      {
+        cover:
+          "https://img-oss.zjseca.com/government/20221122/1669105837384.png",
+        name: "杭州艺福堂茶叶有限公司",
+        type: "普通会员单位",
+        business: "茶叶销售、茶叶制品"
+      }
+    ]
   },
 
   onLoad(options) {},
