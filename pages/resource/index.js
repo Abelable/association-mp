afde744b425f554efe9bf2d13e0d643131e597e4
funguy <1209683835@qq.com>
2025-03-11 17:44:56 +0800

    feat: 资源中心

--- pages/resource/index.js
+++ pages/resource/index.js
@@ -1,5 +1,22 @@
-Page({
-  data: {},
+const { statusBarHeight } = getApp().globalData;
 
-  onLoad(options) {}
+Page({
+  data: {
+    statusBarHeight,
+    curMenuIdx: 0,
+    subMenuList: ["全部", "电商", "直播", "农产品", "AI", "制造业", "机械", "汽车"],
+    curSubMenuIdx: 0
+  },
+
+  onLoad(options) {},
+
+  selectMenu(e) {
+    const curMenuIdx = e.currentTarget.dataset.index;
+    this.setData({ curMenuIdx });
+  },
+
+  selectSubMenu(e) {
+    const curSubMenuIdx = e.currentTarget.dataset.index;
+    this.setData({ curSubMenuIdx });
+  },
 });
