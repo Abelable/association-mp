df42417ad8d429392a760a701cb6f17d16d08ce0
funguy <1209683835@qq.com>
2025-03-12 18:02:45 +0800

    feat: 协会风采

--- pages/index/subpages/course/index.js
+++ pages/index/subpages/course/index.js
@@ -2,6 +2,18 @@ import IndexService from '../../utils/indexService'
 
 Page({
   data: {
+    subMenuList: [
+      "推荐",
+      "培训",
+      "助农",
+      "党建",
+      "AI",
+      "制造业",
+      "机械",
+      "汽车"
+    ],
+    curSubMenuIdx: 0,
+    categoryPickerModalVisible: false,
     courseList: [],
   },
 
@@ -10,10 +22,26 @@ Page({
     wx.setNavigationBarTitle({ title })
     this.id = id
     this.title = title
+
+    this.setCourseList(true)
   },
 
-  onLoad() {
-    this.setCourseList(true)
+  selectSubMenu(e) {
+    const curSubMenuIdx = e.currentTarget.dataset.index;
+    this.setData({ curSubMenuIdx });
+  },
+
+  showCategoryPickerModal() {
+    this.setData({ categoryPickerModalVisible: true });
+  },
+
+  confirmCategoryPick(e) {
+    const curSubMenuIdx = e.detail;
+    this.setData({ curSubMenuIdx, categoryPickerModalVisible: false });
+  },
+
+  hideCategoryPickerModal() {
+    this.setData({ categoryPickerModalVisible: false });
   },
   
   onPullDownRefresh() {
