9a2f285dee4d87002a5c2a90ffc416a2a8728e55
funguy <1209683835@qq.com>
2025-03-12 17:17:50 +0800

    feat: 协会风采

--- pages/activity/subpages/album-detail/index.js
+++ pages/activity/subpages/album-detail/index.js
@@ -1,7 +1,18 @@
+const { statusBarHeight } = getApp().globalData;
+
 Page({
   data: {
+    statusBarHeight,
+    imageList: [
+      "https://static.youbozhenxuan.cn/img/20241030/1730274694559微信图片_20241030151103.jpg",
+      "https://static.youbozhenxuan.cn/img/20250201/1738408261708番茄2.jpg",
+      "https://static.youbozhenxuan.cn/img/20241205/17333876903131.jpg",
+      "https://static.youbozhenxuan.cn/img/20241009/1728445213307微信图片_20241009113503.jpg",
+      "https://static.youbozhenxuan.cn/img/20241220/1734674796055鹿大叔主图2.jpg",
+      "https://static.youbozhenxuan.cn/img/20240921/1726911529427DSC_9755.JPG",
+      "https://static.youbozhenxuan.cn/img/20241015/1728993830969主图2.jpg"
+    ]
   },
 
-  onLoad(options) {
-  }
-})
+  onLoad(options) {}
+});
