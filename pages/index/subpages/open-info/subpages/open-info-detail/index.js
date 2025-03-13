e389f3bfe10837cf35fe5213256d1e0bd8e7a535
funguy <1209683835@qq.com>
2025-03-13 16:58:20 +0800

    feat: 信息公开

--- pages/index/subpages/open-info/subpages/open-info-detail/index.js
+++ pages/index/subpages/open-info/subpages/open-info-detail/index.js
@@ -1,7 +1,7 @@
-import checkLogin from '../../../../../../utils/checkLogin'
-import LowService from '../../utils/lowService'
+import checkLogin from "../../../../../../utils/checkLogin";
+import OpenInfoService from "../../utils/openInfoService";
 
-const lowService = new LowService()
+const openInfoService = new OpenInfoService();
 
 Page({
   data: {
@@ -12,113 +12,117 @@ Page({
   },
 
   onLoad(options) {
-    const { id, scene } = options
-    const decodedScene = scene ? decodeURIComponent(scene) : ''
-    this.id = id || decodedScene.split('-')[0]
-    this.setInfo()
-    wx.showLoading({ title: '加载中...' })
+    const { id, scene } = options;
+    const decodedScene = scene ? decodeURIComponent(scene) : "";
+    this.id = id || decodedScene.split("-")[0];
+    this.setInfo();
+    wx.showLoading({ title: "加载中..." });
   },
 
   async setInfo() {
-    const info = await lowService.getLowDetail(this.id)
-    this.setData({ info })
+    const info = await openInfoService.getOpenInfoDetail(this.id);
+    this.setData({ info });
   },
 
   load() {
-    wx.hideLoading()
+    wx.hideLoading();
   },
 
   async toggleCollect() {
     checkLogin(() => {
-      const { id, is_collect } = this.data.info
-      const status = is_collect == 1 ? 0 : 1
+      const { id, is_collect } = this.data.info;
+      const status = is_collect == 1 ? 0 : 1;
       this.setData({
-        ['info.is_collect']: status
-      })
-      lowService.toggleLowCollectStatus(id, status)
-    })
+        ["info.is_collect"]: status
+      });
+      openInfoService.toggleOpenInfoCollectStatus(id, status);
+    });
   },
 
   async togglePraise() {
     checkLogin(() => {
-      const { id, is_like } = this.data.info
-      const status = is_like == 1 ? 0 : 1
+      const { id, is_like } = this.data.info;
+      const status = is_like == 1 ? 0 : 1;
       this.setData({
-        ['info.is_like']: status
-      })
-      lowService.toggleLowPraiseStatus(id, status)
-    })
+        ["info.is_like"]: status
+      });
+      openInfoService.toggleOpenInfoPraiseStatus(id, status);
+    });
   },
 
   showSharePopup() {
     checkLogin(() => {
       this.setData({
         sharePopupVisible: true
-      })
-      this.setPosterInfo()
-    })
+      });
+      this.setPosterInfo();
+    });
   },
 
   async setPosterInfo() {
-    let { posterBgUrl, posterAvatarUrl } = getApp().globalData
+    let { posterBgUrl, posterAvatarUrl } = getApp().globalData;
     if (!posterBgUrl) {
-      const { path } = await lowService.getImageInfo('https://img.ubo.vip/mp/association/bg.png')
-      posterBgUrl = path
-      getApp().globalData.posterBgUrl = path
+      const { path } = await openInfoService.getImageInfo(
+        "https://img.ubo.vip/mp/association/bg.png"
+      );
+      posterBgUrl = path;
+      getApp().globalData.posterBgUrl = path;
     }
     if (!posterAvatarUrl) {
-      const { path } = await lowService.getImageInfo(wx.getStorageSync('userInfo').avatarUrl)
-      posterAvatarUrl = path
-      getApp().globalData.posterAvatarUrl = path
+      const { path } = await openInfoService.getImageInfo(
+        wx.getStorageSync("userInfo").avatarUrl
+      );
+      posterAvatarUrl = path;
+      getApp().globalData.posterAvatarUrl = path;
     }
 
-    const { id, image, title } = this.data.info
-    const { path: cover } = await lowService.getImageInfo(image)
-    const { app_code } = await lowService.share({ type: 2, legal_id: id })
-    const { path: qrCode } = await lowService.getImageInfo(app_code)
+    const { id, image, title } = this.data.info;
+    const { path: cover } = await openInfoService.getImageInfo(image);
+    const { app_code } = await openInfoService.share({ type: 2, legal_id: id });
+    const { path: qrCode } = await openInfoService.getImageInfo(app_code);
 
     const posterInfo = {
-      bgUrl: posterBgUrl, 
-      avatarUrl: posterAvatarUrl, 
-      nickName: wx.getStorageSync('userInfo').nickName, 
-      cover, 
-      title, 
+      bgUrl: posterBgUrl,
+      avatarUrl: posterAvatarUrl,
+      nickName: wx.getStorageSync("userInfo").nickName,
+      cover,
+      title,
       qrCode
-    }
-    this.setData({ posterInfo })
+    };
+    this.setData({ posterInfo });
   },
 
   hideSharePopup() {
     this.setData({
       sharePopupVisible: false
-    })
+    });
   },
 
   showPosterModal() {
     if (!this.data.posterInfo) {
-      wx.showLoading({ title: '海报生成中... ' })
+      wx.showLoading({ title: "海报生成中... " });
       setTimeout(() => {
-        this.showPosterModal()
-      }, 1000)
+        this.showPosterModal();
+      }, 1000);
     } else {
-      wx.hideLoading()
+      wx.hideLoading();
       this.setData({
         sharePopupVisible: false,
         posterModalVisible: true
-      })
+      });
     }
   },
 
   hidePosterModal() {
     this.setData({
       posterModalVisible: false
-    })
+    });
   },
 
   onShareAppMessage() {
-    const { image, title } = this.data.info
-    const imageUrl = `${image}?x-oss-process=image/resize,m_fill,h_180,w_180`
-    const path = `/pages/index/subpages/low/subpages/low-detail/index?id=${this.id}`
-    return { path, title, imageUrl }
+    const { image, title } = this.data.info;
+    const imageUrl = `${image}?x-oss-process=image/resize,m_fill,h_180,w_180`;
+    const path = `/pages/index/subpages/low/subpages/low-detail/index?id=${this.id}`;
+    return { path, title, imageUrl };
   }
-})
+});
