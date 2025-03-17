722c15d153ac348c13c398fc11bd394295df7c70
funguy <1209683835@qq.com>
2025-03-17 17:57:50 +0800

    feat: 企业名录

--- pages/resource/utils/resourceService.js
+++ pages/resource/utils/resourceService.js
@@ -10,7 +10,8 @@ class ResourceService extends BaseService {
   async getEnterpriseList(categoryId, page, page_size = 10) {
     return await this.get({
       url: `${this.baseUrl}/api/v1/enter-apply/list`,
-      data: { categoryId, page, page_size }
+      data: { categoryId, page, page_size },
+      loadingTitle: "加载中..."
     });
   }
 }
